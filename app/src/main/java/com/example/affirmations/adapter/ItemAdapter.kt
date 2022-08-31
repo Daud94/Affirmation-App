367d8875c372584c03f70a764b8a2a4551b83eba
Daud <olarewajuyahyadaud@gmail.com>
2022-08-31 05:23:01 +0100

    Display a list of images using cards

--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -4,14 +4,16 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
 class ItemAdapter(private val dataset: List<Affirmation>, private val context: Context):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
-    class ItemViewHolder(private val view: View):RecyclerView.ViewHolder(view){
+    class ItemViewHolder(view: View):RecyclerView.ViewHolder(view){
         val textView:TextView= view.findViewById(R.id.item_title)
+        val imageView:ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,6 +25,7 @@ class ItemAdapter(private val dataset: List<Affirmation>, private val context: C
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
          holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 
     override fun getItemCount(): Int {
